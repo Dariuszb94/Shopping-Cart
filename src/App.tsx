f1e7dd4ef60f2e5e899efda57983b55a234cc965
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-10-04 17:42:17 +0200

    home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
 function App() {
   return (
     <Container>
       <Routes>
-        <Route path='/'></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path='/store'></Route>
         <Route path='/about'></Route>
       </Routes>
