7dc7b6f3f5131e2c324ec29104deb579cb37292a
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-10-29 11:39:28 +0200

    change button height

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export function Navbar() {
             About
           </Nav.Link>
         </Nav>
-        <Button style={{ width: '3rem' }}>
+        <Button style={{ width: '3rem', height: '3rem' }}>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 576 512'
