4d7fa0d9564ee1930c0f7d16da69ea74be519651
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-10-29 11:38:55 +0200

    change button width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export function Navbar() {
             About
           </Nav.Link>
         </Nav>
-        <Button>
+        <Button style={{ width: '3rem' }}>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 576 512'
