a1618758d13c4c66315c8029497a74971cdfd3f4
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-11-25 17:20:20 +0100

    navbar sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 export function Navbar() {
   return (
-    <NavbarBs className='bg-white shadow-sm mb-3'>
+    <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
       <Container>
         <Nav className='me-auto'>
           <Nav.Link to='/' as={NavLink}>
