18b8a1817f5d7fbbaec25fdfe9338a938f481509
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-12-16 12:22:02 +0100

    storeItem types

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,9 @@
-export function StoreItem({ id, name, price, imgUrl }) {
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
   return <></>;
 }
